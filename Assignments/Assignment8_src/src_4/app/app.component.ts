43c2529c47d2b27cb09816c17c5721a3f3476eea
SwapnilAnandaZimal <swapnilzimal291199@gmail.com>
2024-01-07 11:49:43 +0530

    Assignment8 solution

---
+++ Assignments/Assignment8_src/src_4/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'EventBinding';
+  public LengthOfName : any;
+
+  public Count(name : any)
+  {
+this.LengthOfName = name.length;
+  }
+}
